746d481ca07c7590eb032f1a536239b03e88c585
liwenwu <dashixiong.lee@gmail.com>
2018-05-09 17:39:25 +0800

    1. modify the Dockerfile based on python:2-alpine to make the image smaller. 2. move the pip packages list into requirements.txt 3. made some changes of echo message.

--- services/goods/dispatcher.py
+++ services/goods/dispatcher.py
@@ -3,10 +3,14 @@
 
 import socket
 
+_VERSION = '0.1'
+
 def getHostIp():
     si = [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]]
     return si[0][1]
 
 class GoodsDispatcher(object):
     def getCategories(self, cid):
-        return [str(cid), 'foo', 'bar', getHostIp()]
+        return [
+            _VERSION, 'getCategories', str(cid), getHostIp()
+        ]
